dfdeb0820af3aac626adaaa59c333026fe064001
zhangkunfeng <291990078@qq.com>
2018-02-10 15:51:12 +0800

    controller修改

--- src/main/java/cn/e3mall/sso/controller/showIndexController.java
+++ src/main/java/cn/e3mall/sso/controller/showIndexController.java
@@ -1,6 +1,7 @@
 package cn.e3mall.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,8 +10,10 @@ public class showIndexController {
 	public String showRegister() {
 		return "register";
 	}
+
 	@RequestMapping("/page/login")
-	public String showLogin(){
+	public String showLogin(String redirect, Model model){
+		model.addAttribute("redirect",redirect);
 		return "login";
 	}
 }
